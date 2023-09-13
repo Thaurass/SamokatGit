9a2db8e53c758de3bc8611cb17ef0a3228c6d6ba
persesma <xj48val@gmail.com>
2023-09-13 14:26:15 +0700

    FirstEdition

---
+++ samokat/Account.cs
@@ -0,0 +1,97 @@
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+
+namespace samokat;
+
+public class Account
+{
+    struct User
+    {
+        public User(string name, string password, int age)
+        {
+            Name = name;
+            Password = password;
+            Age = age;
+            Balance = 0;
+        }
+
+        public string Name { get; }
+        public string Password { get; }
+        public int Age { get; }
+        public double Balance { get; }
+
+        
+        // private override string Register()
+        // {
+        //     return $"({_name}, {_password}, {_age}, {_balance})";
+        // }
+
+    }
+    
+    private static readonly List<User> Users = new();
+    
+    protected internal static void StartScreen()
+    {
+        Console.Clear();
+        Console.WriteLine("Добро пожаловать в сервис самокат");
+        Console.WriteLine("1. Войти");
+        Console.WriteLine("2. Зарегистрироваться");
+        Console.WriteLine("3. Выйти из приложения");
+            
+        bool stop = false;
+        while (!stop)
+        {
+            string? answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "1":
+                    Login();
+                    stop = true;
+                    break;
+                case "2":
+                    Register();
+                    stop = true;
+                    break;
+                case "3":
+                    Environment.Exit(0);
+                    stop = true;
+                    break;
+                default:
+                    Console.WriteLine("Неверный ввод");
+                    break;
+            }
+        }
+    }
+
+    private static void Login()
+    {
+        Console.WriteLine("Введите логин:");
+        string login = Console.ReadLine();
+        Console.WriteLine("Введите пароль:");
+        string password = Console.ReadLine();
+
+        // if (Users.Contains(user => user.Name.Contains(login)))
+        // {
+        //     
+        // }
+
+        //Users.Find(user => user.Name.Contains(login)).Name;
+        
+    }
+    
+    private static void Register()
+    {
+        
+        Console.WriteLine("Придумайте логин:");
+        string login = Console.ReadLine();
+        Console.WriteLine("Придумайте пароль:");
+        string password = Console.ReadLine();
+        Console.WriteLine("Введите свой возраст");
+        string age = Console.ReadLine();
+        
+        Users.Add(new User(login, password, Convert.ToInt32(age)));
+        
+        Console.WriteLine($"Регистрация прошла успешно, {Users.Find(user => user.Name.Contains(login)).Name}");
+        
+    }
+}
---
+++ samokat/Program.cs
@@ -0,0 +1,61 @@
+﻿using static samokat.Account;
+
+namespace samokat // Note: actual namespace depends on the project name.
+{
+    internal abstract class Program
+    {
+        static void Main(string[] args)
+        {
+            StartScreen();
+        }
+
+        
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Главное меню:");
+            Console.WriteLine("Стоимость поездки 5 рублей за одну минуту");
+            Console.WriteLine("1. Арендовать самокат");
+            Console.WriteLine("2. Забронировать самокат");
+            Console.WriteLine("3. Выйти");
+
+            bool stop = false;
+            while (!stop)
+            {
+                string? answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "1":
+                        Rent();
+                        stop = true;
+                        break;
+                    case "2":
+                        Book();
+                        stop = true;
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        stop = true;
+                        break;
+                    default:
+                        Console.WriteLine("Неверный ввод");
+                        break;
+                }
+            }
+
+        }
+
+        static void Rent()
+        {
+            Console.Clear();
+            Console.WriteLine("Введите время аренды самоката:");
+        }
+        
+        static void Book()
+        {
+            Console.Clear();
+            Console.WriteLine("Введите время бронирования самоката:");
+        }
+    }
+}
