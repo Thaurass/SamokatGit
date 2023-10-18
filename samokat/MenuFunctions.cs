4084fcc890c2079ff5720db7f675e08a0b9b39ff
Thaurass <96063091+Thaurass@users.noreply.github.com>
2023-10-18 19:29:00 +0700

    Create Print_message and Read_message functions

--- samokat/MenuFunctions.cs
+++ samokat/MenuFunctions.cs
@@ -10,29 +10,29 @@ namespace samokat
         internal static void StartMenuMsg()
         {
             Console.Clear();
-            Console.WriteLine("Главное меню:");
-            Console.WriteLine("1. Арендовать самокат");
-            Console.WriteLine("2. Профиль");
-            Console.WriteLine("3. Пополнить баланс");
-            Console.WriteLine("4. Ввести промокод");
-            Console.WriteLine("5. Выйти");
+            Print_message("Главное меню:");
+            Print_message("1. Арендовать самокат");
+            Print_message("2. Профиль");
+            Print_message("3. Пополнить баланс");
+            Print_message("4. Ввести промокод");
+            Print_message("5. Выйти");
         }
 
         internal static void Rent()
         {
             Console.Clear();
             Load();
-            Console.WriteLine("Введите номер желаймого самоката:");
-            string Number = Console.ReadLine().ToUpper();
+            Print_message("Введите номер желаймого самоката:");
+            string Number = Read_message().ToUpper();
 
             if (Scooters.Contains(Scooters.Find(Transport => Transport.Number == Number)))
             {
                 CurrentSamokat = Scooters.Find(Transport => Transport.Number == Number);
-                Console.WriteLine("Введите время аренды самоката:");
+                Print_message("Введите время аренды самоката:");
                 int ChargeTm = Convert.ToInt32((CurrentSamokat.Charge / (CurrentSamokat.Speed - 5) / 1000) * 60);
-                Console.WriteLine("Доступное время до " + ChargeTm + " минут");
+                Print_message("Доступное время до " + ChargeTm + " минут");
 
-                if (Int32.TryParse(Console.ReadLine(), out int tm))
+                if (Int32.TryParse(Read_message(), out int tm))
                 {
                     double t = tm;
 
@@ -40,14 +40,14 @@ namespace samokat
                     {
                         if (tm * CurrentSamokat.Costs > CurrentUser.Balance)
                         {
-                            Console.WriteLine("У вас недостаточно средств пополните баланс");
+                            Print_message("У вас недостаточно средств пополните баланс");
                         }
                         if (tm >= ChargeTm)
                         {
-                            Console.WriteLine("Данное время поездки не доступно, введите актуальное " +
-                                              "или возбмите другой самокат");
+                            Print_message("Данное время поездки не доступно, введите актуальное " +
+                                              "или возьмите другой самокат");
                         }
-                        Console.WriteLine("Нажмите чтобы продолжить");
+                        Print_message("Нажмите чтобы продолжить");
                         Console.ReadKey();
                         Menu();
                     }
@@ -58,24 +58,24 @@ namespace samokat
                         CurrentSamokat.Charge -= (int)((double)(t / 60) * (CurrentSamokat.Speed - 5) * 1000);
                         Scooters[CurrentSamokat.Index] = CurrentSamokat;
 
-                        Console.WriteLine("Цена вашей поездки составить " + tm * CurrentSamokat.Costs);
+                        Print_message("Цена вашей поездки составить " + tm * CurrentSamokat.Costs);
                         CurrentUser.Balance = CurrentUser.Balance - tm * CurrentSamokat.Costs;
-                        Console.WriteLine("Хорошей поездки!");
+                        Print_message("Хорошей поездки!");
 
                     }
                 }
                 else
                 {
-                    Console.WriteLine($"Время аренды должно быть в формате числа");
+                    Print_message($"Время аренды должно быть в формате числа");
                 }
 
             }
             else
             {
-                Console.WriteLine("Такой самокат отсутствует");
+                Print_message("Такой самокат отсутствует");
             }
 
-            Console.WriteLine("Нажмите чтобы продолжить");
+            Print_message("Нажмите чтобы продолжить");
             Console.ReadKey();
             Menu();
         }
@@ -83,25 +83,25 @@ namespace samokat
         internal static void Profile()
         {
             Console.Clear();
-            Console.WriteLine("Здравствуйте: " + CurrentUser.Name);
-            Console.WriteLine("Ваш возраст: " + CurrentUser.Age);
-            Console.WriteLine("Ваш баланс: " + CurrentUser.Balance);
-            Console.WriteLine("Ваш пароль: " + CurrentUser.Password);
+            Print_message("Здравствуйте: " + CurrentUser.Name);
+            Print_message("Ваш возраст: " + CurrentUser.Age);
+            Print_message("Ваш баланс: " + CurrentUser.Balance);
+            Print_message("Ваш пароль: " + CurrentUser.Password);
 
             if (CurrentUser.PromotionalCode != "")
             {
                 Console.Write("Ваш промокод ");
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("активирован");
+                Print_message("активирован");
                 Console.ResetColor();
 
             }
 
             Print_Stat();
-            Console.WriteLine("Вы проехали: {0:F2} км", CurrentUser.Distance / 1000);
-            Console.WriteLine("Время в пути вместе: {0:#} минут", CurrentUser.Time);
+            Print_message("Вы проехали: {0:F2} км", CurrentUser.Distance / 1000);
+            Print_message("Время в пути вместе: {0:#} минут", CurrentUser.Time);
 
-            Console.WriteLine("Нажмите чтобы выйти");
+            Print_message("Нажмите чтобы выйти");
             Console.ReadKey();
             Menu();
         }
@@ -110,18 +110,18 @@ namespace samokat
 
         internal static void ChangePromo()
         {
-            Console.WriteLine("Введите промокод:");
-            string temp = Console.ReadLine();
+            Print_message("Введите промокод:");
+            string temp = Read_message();
 
             if (temp.Contains(promo) || temp.ToUpper().Contains(promo))
             {
                 CurrentUser.PromotionalCode = temp;
                 Print_Promo();
-                Console.WriteLine("Нажмите чтобы выйти");
+                Print_message("Нажмите чтобы выйти");
             }
             else
             {
-                Console.WriteLine("Неверный промокод");
+                Print_message("Неверный промокод");
             }
 
             Console.ReadKey();
@@ -131,29 +131,44 @@ namespace samokat
         internal static void Deposit()
         {
             Console.Clear();
-            Console.WriteLine("Ваш баланс:" + CurrentUser.Balance);
-            Console.WriteLine("Какую сумму хотите ввести?");
+            Print_message("Ваш баланс:" + CurrentUser.Balance);
+            Print_message("Какую сумму хотите ввести?");
 
-            if (Double.TryParse(Console.ReadLine(), out double sum))
+            if (Double.TryParse(Read_message(), out double sum))
             {
                 CurrentUser.Balance += sum;
-                Console.WriteLine("Баланс успешно пополнен, теперь ваш баланс составляет: " + CurrentUser.Balance);
+                Print_message("Баланс успешно пополнен, теперь ваш баланс составляет: " + CurrentUser.Balance);
             }
             else
             {
-                Console.WriteLine("неверно введена сумма, ваш баланс составляет: " + CurrentUser.Balance);
+                Print_message("неверно введена сумма, ваш баланс составляет: " + CurrentUser.Balance);
             }
 
 
-            Console.WriteLine("Нажмите чтобы продолжить");
+            Print_message("Нажмите чтобы продолжить");
 
             Console.ReadKey();
             Menu();
         }
 
+        internal static void Print_message(string _yourMessage)
+        {
+            Console.WriteLine(_yourMessage);
+        }
+
+        internal static void Print_message(string _yourMessage, double data) 
+        {
+            Console.WriteLine(_yourMessage, data);
+        }
+
+        internal static string Read_message()
+        {
+            return Console.ReadLine();
+        }
+
         static void Print_Stat()
         {
-            Console.WriteLine("#######################################\r\n" +
+            Print_message("#######################################\r\n" +
                               "#                                     #\r\n" +
                               "#   #####   ######     ##     ######  #\r\n" +
                               "#  ##   ##  # ## #    ####    # ## #  #\r\n" +
@@ -169,7 +184,7 @@ namespace samokat
         static void Print_Promo()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(
+            Print_message(
               "##################################################################################################################\r\n" +
               "#                                                                                                                #\r\n" +
               "#  ######   ######    #####   ##   ##   #####              ##       ####   ######    ####    ##   ##   #######   #\r\n" +
